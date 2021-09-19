c6166a896e5bb6689075d4944aaa639b6c319506
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-19 11:56:28 -0400

    Committing #2

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selection sort of fixed size array
+ * @array: Array of data
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp, n;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		n = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[n])
+				n = j;
+	}
+
+	if (n != i)
+	{
+		temp = array[i];
+		array[i] = array[n];
+		array[n] = temp;
+		print_array(array, size);
+	}
+}
