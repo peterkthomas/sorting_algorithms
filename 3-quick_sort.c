ea9fdc09b9b51710d7696391790c5cfeb82ee0b2
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-19 12:31:19 -0400

    3 initial commit

---
+++ 3-quick_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * array_split - splits an array
+ * @array: array of data
+ * @size: size of array
+ * @left: left index
+ * @right: right index
+ */
+int array_split(int *array, size_t size, unsigned int left, unsigned int right)
+{
+	unsigned int i, j, pivot, temp;
+
+	pivot = array[right];
+	i = left;
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				temp = array[j];
+				array[j] = 	array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (array[i] != arr[right])
+	{
+		temp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = temp;
+		print_array(arr, size);
+	}
+
+	return (i);
+}
+/**
+ * array_sort - sorts data recursively
+ * array: array of data
+ * size: size of the array
+ * @left: index of the left side of pivot
+ * @right: index of the right side of pivot
+ */
+void array_sort(int *array, size_t size, unsigned int left, unsigned int right)
+{
+	unsigned int pivot;
+
+	if (left < right)
+	{
+		pivot = array_split(array, size, left, right);
+		array_sort(array, size, left, pivot - 1);
+		array_sort(array, size, pivot + 1, right);
+	}
+}
+/**
+ * quick_sort - implementation of a quick sort
+ * @array: array of data
+ * @size: array's size
+ */
+void quick_sort (int *array, size_t size)
+{
+	unsigned int left = 0, right = size - 1;
+
+	if (!array || size < 2)
+		return;
+
+	array_sort (array, size, left, right);
+
+}
